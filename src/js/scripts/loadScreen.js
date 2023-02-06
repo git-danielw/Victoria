a11107f92c27b9c71bdb87f0e2d1df640566b7a8
Daniel <214629@ais.edu.hk>
2023-02-07 00:56:52 +0800

    final fixes

--- src/js/scripts/loadScreen.js
+++ src/js/scripts/loadScreen.js
@@ -1,32 +1,24 @@
 let navBarGenerated = false;
 if (debug) document.querySelector(':root').style.setProperty('--animation-speed', `${debugOptions.animationSpeed}s`);
 
-let video = document.getElementsByClassName('headerVideo')[0];
-let loadedVideo = false;
-
 setTimeout(() => {
-    video.oncanplaythrough = () => {
-        if (!loadedVideo) {
-            loadedVideo = true;
-            const waitUntilLoad = setInterval(() => {
-                if (loadFetched) {
-                    clearInterval(waitUntilLoad);
-                    document.getElementById('load').classList.add('hide');
-                    if (fetchedNavBar) loadNavbar();
-                    setTimeout(() => {
-                        document.getElementById('load').style.display = 'none';
-                        if (fetchedNavBar && !navBarGenerated) loadNavbar();
-                        if (!fetchedNavBar) {
-                            const waitUntilRun = setInterval(() => {
-                                if (fetchedNavBar) {
-                                    loadNavbar();
-                                    clearInterval(waitUntilRun);
-                                }
-                            }, (debug ? 10 : 300));
+    const waitUntilLoad = setInterval(() => {
+        if (loadFetched) {
+            clearInterval(waitUntilLoad);
+            document.getElementById('load').classList.add('hide');
+            if (fetchedNavBar) loadNavbar();
+            setTimeout(() => {
+                document.getElementById('load').style.display = 'none';
+                if (fetchedNavBar && !navBarGenerated) loadNavbar();
+                if (!fetchedNavBar) {
+                    const waitUntilRun = setInterval(() => {
+                        if (fetchedNavBar) {
+                            loadNavbar();
+                            clearInterval(waitUntilRun);
                         }
-                    }, (debug ? 10 : 1000));
+                    }, (debug ? 10 : 300));
                 }
-            }, (debug ? 10 : 400));
+            }, (debug ? 10 : 1000));
         }
-    }
+    }, (debug ? 10 : 400));
 }, (debug ? 10 : 2000));
